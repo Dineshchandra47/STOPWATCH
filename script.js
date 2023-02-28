a62a4bd9e22002d031144b7e403a5c6840d14b53
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2023-02-28 19:26:48 +0530

    Updated STOPWATCH

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const timer = document.querySelector(".stopwatch");
-// const start = document.getElementById("start");
-// const pause = document.getElementById("pause");
-// const reset = document.getElementById("reset");
-// const cast = document.getElementById("cast");
 const castDivision = document.querySelector(".time");
-
 let hour = 0,
   minutes = 0,
   seconds = 0,
@@ -13,9 +8,7 @@ let hour = 0,
 let intervelID = null;
 
 let count = 1;
-function startTimer(){
-// start.onclick = () => {
-  // console.log(intervelID);
+function startTimer() {
   if (!intervelID) {
     intervelID = setInterval(() => {
       if (minutes === 60) {
@@ -34,20 +27,22 @@ function startTimer(){
       timer.textContent = `${hour < 10 ? "0" + hour : hour} : ${
         minutes < 10 ? "0" + minutes : minutes
       } : ${seconds < 10 ? "0" + seconds : seconds} : ${
-        millisec < 10 ? "00" + millisec : millisec < 100 ? "0" + millisec : millisec
+        millisec < 10
+          ? "00" + millisec
+          : millisec < 100
+          ? "0" + millisec
+          : millisec
       }`;
     }, 10);
   }
-};
+}
 function pauseTimer() {
-// pause.onclick = () => {
   if (intervelID) {
     clearInterval(intervelID);
     intervelID = null;
   }
-};
-function resetTimer(){
-// reset.onclick = () => {
+}
+function resetTimer() {
   if (intervelID) {
     clearInterval(intervelID);
     intervelID = null;
@@ -59,17 +54,22 @@ function resetTimer(){
   timer.textContent = "00 : 00 : 00 : 000";
   castDivision.innerHTML = "";
   count = 1;
-};
+}
 
-function castTimer(){
-// cast.onclick = () => {
+function castTimer() {
   let p = document.createElement("p");
   p.clasecondsName = "cast-item";
   p.textContent =
     `CAST ${count} : ` +
-    `${hour < 10 ? "0" + hour : hour} : ${minutes < 10 ? "0" + minutes : minutes} : ${
-      seconds < 10 ? "0" + seconds : seconds
-    } : ${millisec < 10 ? "00" + millisec : millisec < 100 ? "0" + millisec : millisec}`;
+    `${hour < 10 ? "0" + hour : hour} : ${
+      minutes < 10 ? "0" + minutes : minutes
+    } : ${seconds < 10 ? "0" + seconds : seconds} : ${
+      millisec < 10
+        ? "00" + millisec
+        : millisec < 100
+        ? "0" + millisec
+        : millisec
+    }`;
   count++;
   castDivision.appendChild(p);
-};
+}
